fb645fba5209f1834ed3da1622fefb855355a7b5
eternaltung <eternaltung@gmail.com>
2018-06-10 22:47:27 +0800

    add question 844 code

--- LeetCodePractice/844_Backspace String Compare/BackspaceStringCompare.cs
+++ LeetCodePractice/844_Backspace String Compare/BackspaceStringCompare.cs
@@ -9,6 +9,37 @@ namespace LeetCodePractice._844_Backspace_String_Compare
 		[TestMethod]
 		public void TestMethod1()
 		{
+			Assert.IsTrue(BackspaceCompare("ab#c", "ad#c"));
+			Assert.IsTrue(BackspaceCompare("ab##", "c#d#"));
+			Assert.IsTrue(BackspaceCompare("a##c", "#a#c"));
+			Assert.IsFalse(BackspaceCompare("a#c", "b"));
+		}
+
+		public bool BackspaceCompare(string S, string T)
+		{
+			do
+			{
+				var index = S.IndexOf('#');
+				if (index < 0)
+					break;
+
+				S = S.Remove(index, 1);
+				if (index > 0)
+					S = S.Remove(index - 1, 1);
+			} while (true);
+
+			do
+			{
+				var index = T.IndexOf('#');
+				if (index < 0)
+					break;
+
+				T = T.Remove(index, 1);
+				if (index > 0)
+					T = T.Remove(index - 1, 1);
+			} while (true);
+
+			return S == T;
 		}
 	}
 }
